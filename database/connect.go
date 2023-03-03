25102eff01f279e05a42fc529cf2a76a0345cf5c
JosseMontano <josemariazambranamontano123@gmail.com>
2023-03-03 11:46:14 -0400

    Add variables of enviroment

--- database/connect.go
+++ database/connect.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"github.com/JosseMontano/estateInTheCloud/models"
+	"github.com/JosseMontano/estateInTheCloud/utils"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -9,12 +10,13 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	host := "localhost"
-	user := "postgres"
-	password := "8021947cbba"
-	dbname := "realEstatePrueba1"
-	port := "5432"
-
+	
+ 	host := utils.DotEnvVariable("HOST")
+	user := utils.DotEnvVariable("USER")
+	password := utils.DotEnvVariable("PASSWORD")
+	dbname := utils.DotEnvVariable("DBNAME")
+	port := utils.DotEnvVariable("PORT")
+ 
 	DSN := "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port
 
 	database, err := gorm.Open(postgres.Open(DSN), &gorm.Config{})
